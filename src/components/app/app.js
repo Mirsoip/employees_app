99edcd0bbe6f63ac1348a9ba567065b95b1181dd
Mirsoip <mirsoip.adilov@gmail.com>
2022-05-16 11:38:17 +0500

    Lesson 135 without homework

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,24 +4,42 @@ import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
+import { Component } from 'react';
 
-function App() {
-    const data = [
-        {name: "Fillip Kirkorov", salary: 500000, increase: false, id: 1},
-        {name: "Valeriy Meladze", salary: 350000, increase: false, id: 2},
-        {name: "Dima Bilan", salary: 300000, increase: true, id: 3}
-    ];
-    return(
-        <div className="app">
-            <AppInfo />
-            <div className="search-panel">
-                <SearchPanel />
-                <AppFilter />
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: "Fillip Kirkorov", salary: 500000, increase: false, id: 1},
+                {name: "Valeriy Meladze", salary: 350000, increase: false, id: 2},
+                {name: "Dima Bilan", salary: 300000, increase: true, id: 3}
+            ]
+        }
+    }
+
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    render() {
+        
+        return(
+            <div className="app">
+                <AppInfo />
+                <div className="search-panel">
+                    <SearchPanel />
+                    <AppFilter />
+                </div>
+                <EmployeesList data={this.state.data} onDelete={this.deleteItem} />
+                <EmployeesAddForm />
             </div>
-            <EmployeesList data={data} />
-            <EmployeesAddForm />
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
